4c1168cd560c04d0411146d8261e15419db827e7
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-16 10:50:45 +0100

    job05 done

---
+++ jour1/job05/script.js
@@ -0,0 +1,7 @@
+function afficherjourssemaines() {
+    const jourssemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+    for (let i = 0; i < jourssemaines.length; i++) {
+        console.log(jourssemaines[i]);
+    }
+}
+console.log(afficherjourssemaines());
