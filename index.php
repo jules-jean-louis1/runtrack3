08f093e428218abb8f10076a1791b34faa4348be
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-16 09:58:58 +0100

    1er

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job01</title>
+</head>
+<body>
+    <script>alert("Ceci est un message d'alerte!")</script>
+</body>
+</html>
