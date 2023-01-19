472c246b568b1ffe57250a901dcd40a76b884cbd
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-19 22:01:32 +0100

    job03 done

--- jour4/job03/ss.js
+++ jour4/job03/ss.js
@@ -18,15 +18,14 @@ $(document).ready(function()
         }
         
         function toHTML(json) {
-            /* json.forEach((item) => {
-                result.append(`<p>ID: ${item.id} Type: ${item.type}</p>`);
-            }); */
             for (let i = 0; i<json.length; i++) {
                 const item = json[i];
                 if (item.id == id && nom == item.name.english || nom == item.name.japanese || nom == item.name.chinese || nom == item.name.french) {
                     result.append(`<p class="alert alert-success">
                     ID: ${item.id} Type: ${item.type} Nom: French ${item.name.french}, English ${item.name.english}, Japanese ${item.name.japanese}, Chinese ${item.name.chinese}
                     </p>`);
+                } else {
+                    result.append(`<p class="alert alert-danger">Les Filtres indiquées ne correspondent à aucun Pokémon.</p>`);
                 }
                 
             } 
