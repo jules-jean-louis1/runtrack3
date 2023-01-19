6b24a053db2805fb9bd023b970772d019a6a9052
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-19 15:48:32 +0100

    change job03

--- jour4/job03/script.js
+++ jour4/job03/script.js
@@ -4,25 +4,38 @@ $(document).ready(function()
     
     $("#submit").on('click', function(event)
     {
-        const getId = $('#getid').val();
-        const getNom = $('#username').val();
-        const getTypes = $('#type_liste').val();
+        const id = $('#getid').val();
+        const nom = $('#username').val();
+        const type = $('#type_liste').val();
         event.preventDefault();
         
-        
-        
-    });
-    async function fetchPokemon() {
-        const r = await fetch ('pokemon.json');
-        if (r.ok === true) {
-            return r.json();
-        } else {
-            throw new Error('Erreur')
+        async function fetchPokemon() {
+            const r = await fetch ('pokemon.json');
+            if (r.ok === true) {
+                return r.json();
+            } else {
+                throw new Error('Erreur')
+            }
         }
-    }
-    fetchPokemon().then(function(data) {
-        console.log(data);
-    }).catch(function(error) {
-        console.log(error);
+        fetchPokemon().then(data => {
+            let filterData = data.filter(item => {
+                if (id && item.id !== id) {
+                    return false;
+                  }
+                  if (nom && item.nom !== nom) {
+                    return false;
+                  }
+                  if (type && item.type !== type) {
+                    return false;
+                  }
+                  return true;
+                });
+                let resultHTML = "";
+                filteredData.forEach(item => {
+                  resultHTML += `<p>ID: ${item.id} Nom: ${item.nom} Type: ${item.type}</p>`;
+                });
+          
+                document.getElementById("result").innerHTML = resultHTML;
+              });
     });
 });
