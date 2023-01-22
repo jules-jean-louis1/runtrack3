b56a5420ecab93bb83b029b8b8f74d8fa0053d7f
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-22 22:47:58 +0100

    test

---
+++ jour5/jour01/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- JS -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Job01-jour05</title>
+</head>
+
+
+<body>
+    <div id="container">
+        <h1>Job01-jour05</h1>
+        <h2>Fetch-Module de connexion</h2>
+        <div id="boutons">
+            <button id="inscription">Inscription</button>
+            <button id="connexion">Connexion</button>
+        </div>
+
+        <div id="content">
+
+        </div>
+
+        <div id="message">
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
