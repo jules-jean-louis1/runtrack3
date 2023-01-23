7f1582b6f513dcad00921f2725fbd1db620c0735
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-23 13:44:07 +0100

    job 04 done

---
+++ jour6/job4/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- JS -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script> 
+    <link rel="stylesheet" href="style.css">
+    <title>Job 02</title>
+</head>
+<body>
+    <header>
+    <nav id="main-menu">
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Présentation</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+        <input type="checkbox" id="hamburger-input" class="burger-shower" />
+        <label id="hamburger-menu" for="hamburger-input">
+            <nav id="sidebar-menu">
+                <h3>Menu</h3>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Présentation</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </label>
+    <div class="overlay"></div>
+    </header>
+    <main>
+    <!-- <article>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+    </article> -->
+    </main>
+</body>
+</html>
