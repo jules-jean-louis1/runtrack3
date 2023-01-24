5afd9d3497ae1ff9d9691dc9a8fcc1376c10005a
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-24 10:58:00 +0100

    1er commit job01 jour7

---
+++ jour7/job01/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>J07 - Job01</title>
+</head>
+<body>
+    <header>
+        <nav class="relative w-full flex flex-warp items-center justify-beteween bg-black py-2 text-orange-400">
+            <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+                <li class="nav-item p-2">
+                    <a href="index.php" class="nav-link focus:text-orange-400 hover:text-orange-700 ease-in duration-100 text-lg font-bold">
+                        Accueil
+                    </a>
+                    <a href="index.php" class="nav-link focus:text-orange-400 hover:text-orange-700 ease-in duration-100 text-lg font-bold">
+                        Inscription
+                    </a>
+                    <a href="index.php" class="nav-link focus:text-orange-400 hover:text-orange-700 ease-in duration-100 text-lg font-bold">
+                        Connexion
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
